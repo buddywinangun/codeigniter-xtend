35cdd6a91491cc123197d9429763a7e8e7642fc4
buddywinangun <buddywinangun@gmail.com>
2018-01-13 14:10:03 +0700

    fixed: util filehelper

--- src/Xtend/Util/FileHelper.php
+++ src/Xtend/Util/FileHelper.php
@@ -16,7 +16,8 @@ final class FileHelper
    * @param int $mode
    * @return void
    */
-  public static function makeDirectory(string $dir, int $mode = 0755): bool {
+  public static function makeDirectory(string $dir, int $mode = 0755): bool
+  {
     // If the directory already exists, do nothing.
     if (file_exists($dir))
       return false;
@@ -41,7 +42,8 @@ final class FileHelper
    * @param string $dstPath
    * @return void
    */
-  public static function copyFile(string $srcPath, string $dstPath, $group = null, $user = null) {
+  public static function copyFile(string $srcPath, string $dstPath, $group = null, $user = null)
+  {
     if (!file_exists($srcPath))
       throw new \RuntimeException('Not found file ' . $srcPath);
     else if (!is_file($srcPath))
@@ -64,7 +66,8 @@ final class FileHelper
    * @param string $dstDir
    * @return void
    */
-  public static function copyDirectory(string $srcDir, string $dstDir) {
+  public static function copyDirectory(string $srcDir, string $dstDir)
+  {
     if (!file_exists($srcDir))
       throw new \RuntimeException('Not found directory ' . $srcDir);
     else if (!is_dir($srcDir))
@@ -86,9 +89,75 @@ final class FileHelper
    * @param string $path
    * @return  void
    */
-  public static function replace(string $path, array $replace) {
+  public static function replace(string $path, array $replace)
+  {
     $content = file_get_contents($path);
     $content = str_replace(array_keys($replace), array_values($replace), $content);
     file_put_contents($path, $content, LOCK_EX);
+  }
+
+  /**
+   * Delete directory or file.
+   *
+   * ```php
+   * use \X\Util\FileHelper;
+   *
+   * // Delete all files and folders in "/ path"..
+   * FileHelper::delete('/test');
+   *
+   * // Delete all files and folders in the "/ path" folder and also in the "/ path" folder.
+   * $deleteSelf = true;
+   * FileHelper::delete('/test', $deleteSelf);
+   *
+   * // Lock before deleting, Locks are disabled by default.
+   * $deleteSelf = true;
+   * $enableLock = true;
+   * FileHelper::delete('/test', $deleteSelf, $enableLock);
+   * ```
+   */
+  public static function delete(...$paths)
+  {
+    if (is_array(reset($paths)))
+      $paths = reset($paths);
+    $deleteSelf = true;
+    $enableLock = false;
+    if (count($paths) > 2 && is_bool(end($paths)) && is_bool($paths[count($paths) - 2])) {
+      $enableLock = end($paths);
+      unset($paths[count($paths) - 1]);
+      $deleteSelf = end($paths);
+      unset($paths[count($paths) - 1]);
+    } else if (count($paths) > 1 && is_bool(end($paths))) {
+      $deleteSelf = end($paths);
+      unset($paths[count($paths) - 1]);
+    }
+    foreach ($paths as $path) {
+      if (!file_exists($path))
+        continue;
+      if (is_file($path))
+        unlink($path);
+      else {
+        $it = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path, \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::CHILD_FIRST);
+        foreach ($it as $info) {
+          if ($info->isDir())
+            rmdir($info);
+          else {
+            if ($enableLock) {
+              // 'w' mode truncates file, you don't want to do that yet!
+              $fp = fopen($info->getPathname(), 'c');
+              flock($fp, LOCK_EX);
+              ftruncate($fp, 0);
+              fclose($fp);
+              unlink($info->getPathname());
+            } else
+              unlink($info);
+          }
+        }
+        if ($deleteSelf) {
+          // Clear the cache of file statuses.
+          clearstatcache();
+          rmdir($path);
+        }
+      }
+    }
   }
 }
