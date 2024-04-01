cbbce0baa470e37ea59959898f5799ad45335596
buddywinangun <buddywinangun@gmail.com>
2024-04-01 09:03:38 +0700

    perf: improve configuration

--- starter/application/config/config.php
+++ starter/application/config/config.php
@@ -23,7 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = (is_https() ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+if (is_cli()) {
+  $config['base_url'] = '';
+} else {
+  $config['base_url'] = (is_https() ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+}
+
 
 /*
 |--------------------------------------------------------------------------
